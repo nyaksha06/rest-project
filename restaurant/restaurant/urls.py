10ad7de43d4ad79d65539afe1a35bba0ab4c4f18
nyaksha06 <nyaksha06@gmail.com>
2024-08-25 00:48:32 +0530

    change in url file

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
     path('orders/', include('order.urls')),
-    path('payments/', include('payments.urls')),
+    path('payments/', include('payment.urls')),
 ]
