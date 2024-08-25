47e5554ec40a74c3d9c580febd7ac4efd852ccac
nyaksha06 <nyaksha06@gmail.com>
2024-08-26 00:26:30 +0530

    models created in menu app

--- restaurant/menu/models.py
+++ restaurant/menu/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Catagory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank = True , null = True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class MenuItem(models.Model):
+    catagory = models.ForeignKey(Catagory , on_delete= models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank = True , null = True)
+    price = models.DecimalField(max_digits =6, decimal_places=2)
+    available = models.BooleanField(default = True)
+
+
+    def __str__(self):
+        return self.name
+    
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
-    path('orders/', include('order.urls')),
-    path('payments/', include('payment.urls')),
+    path('order/', include('order.urls')),
+    path('payment/', include('payment.urls')),
 ]
